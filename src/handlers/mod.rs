bffa44019ae7279d9075e5565d583d95c4ddb4a2
Harmoniousmoss <syaefulbahri@protonmail.com>
2024-07-20 19:06:58 +0700

    add delete blacklisted by id

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,2 +1,4 @@
 pub mod blacklist_handler;
-pub use blacklist_handler::{add_blacklist_ip, get_all_blacklist_ip, get_blacklist_ip_by_id};
+pub use blacklist_handler::{
+    add_blacklist_ip, delete_blacklist_ip_by_id, get_all_blacklist_ip, get_blacklist_ip_by_id,
+};
