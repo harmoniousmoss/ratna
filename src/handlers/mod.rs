a5246206c0ff7aee812ce8f4f07010e97c3c437f
Harmoniousmoss <syaefulbahri@protonmail.com>
2024-07-20 18:59:50 +0700

    Refactor handler name

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,2 +1,2 @@
 pub mod blacklist_handler;
-pub use blacklist_handler::{add_blacklisted_ip, get_all_blocked_ips};
+pub use blacklist_handler::{add_blacklist_ip, get_all_blacklist_ip, get_blacklist_ip_by_id};
