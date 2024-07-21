cae62d0c7553d526cd61ff00f91be565bf4e2aea
Harmoniousmoss <syaefulbahri@protonmail.com>
2024-07-21 09:25:50 +0700

    add post malicious url sub module

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -2,3 +2,6 @@ pub mod blacklist_handler;
 pub use blacklist_handler::{
     add_blacklist_ip, delete_blacklist_ip_by_id, get_all_blacklist_ip, get_blacklist_ip_by_id,
 };
+
+pub mod malicious_handler;
+pub use malicious_handler::add_blacklist_url;
