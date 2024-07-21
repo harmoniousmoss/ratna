55335cd8024484e6dfe58cfd5dbce6853d8bab9d
Harmoniousmoss <syaefulbahri@protonmail.com>
2024-07-21 10:13:30 +0700

    Add sub module get all malicious url

--- src/handlers/malicious_handler.rs
+++ src/handlers/malicious_handler.rs
@@ -1,6 +1,11 @@
 use crate::models::MaliciousUrl;
 use actix_web::{web, HttpResponse, Responder};
-use mongodb::{bson::doc, Client, Collection};
+use futures::stream::StreamExt;
+use mongodb::{
+    bson::{self, doc},
+    options::FindOptions,
+    Client, Collection,
+};
 
 use serde::Deserialize;
 
@@ -27,4 +32,30 @@ pub async fn add_blacklist_url(
             .json("URL successfully added to blacklist with status 'blocked'"),
         Err(e) => HttpResponse::InternalServerError().json(e.to_string()),
     }
+}
+
+// Get all blocked URLs
+pub async fn get_all_blacklist_url(db_client: web::Data<Client>) -> impl Responder {
+    let collection: Collection<MaliciousUrl> = db_client
+        .database("rustkeeper")
+        .collection("malicious_urls");
+
+    let filter = bson::doc! { "status": "blocked" };
+    let find_options = FindOptions::builder()
+        .sort(bson::doc! { "created_at": -1 })
+        .build();
+    let mut cursor = match collection.find(filter, find_options).await {
+        Ok(cursor) => cursor,
+        Err(e) => return HttpResponse::InternalServerError().json(e.to_string()),
+    };
+
+    let mut results: Vec<MaliciousUrl> = Vec::new();
+    while let Some(result) = cursor.next().await {
+        match result {
+            Ok(document) => results.push(document),
+            Err(e) => return HttpResponse::InternalServerError().json(e.to_string()),
+        }
+    }
+
+    HttpResponse::Ok().json(results)
 }
