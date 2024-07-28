5bc13a9d84cb2bb35ffd67e06caaef4df0396363
Harmoniousmoss <syaefulbahri@protonmail.com>
2024-07-28 15:07:00 +0700

    add edit blacklist ip endpoint

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,6 +1,7 @@
 pub mod blacklist_handler;
 pub use blacklist_handler::{
-    add_blacklist_ip, delete_blacklist_ip_by_id, get_all_blacklist_ip, get_blacklist_ip_by_id,
+    add_blacklist_ip, delete_blacklist_ip_by_id, edit_blacklist_ip_by_id, get_all_blacklist_ip,
+    get_blacklist_ip_by_id,
 };
 
 pub mod malicious_handler;
