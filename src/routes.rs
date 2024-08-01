fb9663b2b0728b20472ee411b6df63ebdc40d717
Harmoniousmoss <syaefulbahri@protonmail.com>
2024-08-02 06:55:48 +0700

    Add dedicated file to handle routes

---
+++ src/routes.rs
@@ -0,0 +1,47 @@
+// src/routes.rs
+
+use actix_web::{web, HttpResponse};
+
+use crate::handlers::{
+    add_blacklist_ip, add_blacklist_url, delete_blacklist_ip_by_id, delete_blacklist_url_by_id,
+    edit_blacklist_ip_by_id, edit_blacklist_url_by_id, get_all_blacklist_ip, get_all_blacklist_url,
+    get_blacklist_ip_by_id, get_blacklist_url_by_id, signin, signup,
+};
+
+pub fn configure_routes(cfg: &mut web::ServiceConfig) {
+    cfg
+        // Blacklist IP endpoints
+        .service(
+            web::resource("/blacklist-ip")
+                .route(web::post().to(add_blacklist_ip))
+                .route(web::get().to(get_all_blacklist_ip)),
+        )
+        .service(
+            web::resource("/blacklist-ip/{id}")
+                .route(web::get().to(get_blacklist_ip_by_id))
+                .route(web::delete().to(delete_blacklist_ip_by_id))
+                .route(web::put().to(edit_blacklist_ip_by_id)),
+        )
+        // Blacklist URL endpoints
+        .service(
+            web::resource("/blacklist-url")
+                .route(web::post().to(add_blacklist_url))
+                .route(web::get().to(get_all_blacklist_url)),
+        )
+        .service(
+            web::resource("/blacklist-url/{id}")
+                .route(web::get().to(get_blacklist_url_by_id))
+                .route(web::delete().to(delete_blacklist_url_by_id))
+                .route(web::put().to(edit_blacklist_url_by_id)),
+        )
+        // User endpoints
+        .service(web::resource("/signup").route(web::post().to(signup)))
+        .service(web::resource("/signin").route(web::post().to(signin)));
+}
+
+pub fn configure_greet(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::resource("/")
+            .route(web::get().to(|| async { HttpResponse::Ok().body("Brigatory Here") })),
+    );
+}
