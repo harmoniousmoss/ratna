76cb19127cb7aedcba13bd965e744bd3f1151d01
Harmoniousmoss <syaefulbahri@protonmail.com>
2024-08-02 15:37:45 +0700

    Add blacklist url checking

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -7,7 +7,7 @@ pub use blacklist_handler::{
 pub mod malicious_handler;
 pub use malicious_handler::{
     add_blacklist_url, delete_blacklist_url_by_id, edit_blacklist_url_by_id, get_all_blacklist_url,
-    get_blacklist_url_by_id,
+    get_blacklist_url_by_id, is_blacklist_url,
 };
 
 pub mod brigatory_users_handler;
