d2c0f427b8efcdb3a1844bc312e3d1c3f44abe84
Harmoniousmoss <syaefulbahri@protonmail.com>
2024-08-03 06:52:38 +0700

    Refactor

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,7 +1,7 @@
 pub mod blacklist_handler;
 pub use blacklist_handler::{
     add_blacklist_ip, delete_blacklist_ip_by_id, edit_blacklist_ip_by_id, get_all_blacklist_ip,
-    get_blacklist_ip_by_id,
+    get_blacklist_ip_by_id, is_blacklist_ip,
 };
 
 pub mod malicious_handler;
