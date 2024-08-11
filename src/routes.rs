42f77e2a95e17872fd011390e02a68fecd14e606
Harmoniousmoss <syaefulbahri@protonmail.com>
2024-08-11 14:10:22 +0700

    Add rate limit

--- src/routes.rs
+++ src/routes.rs
@@ -1,39 +1,51 @@
-use crate::middleware::jwt_auth::JwtAuth;
 use actix_web::{web, HttpResponse};
 
 use crate::handlers::{
-    add_blacklist_ip, add_blacklist_url, delete_blacklist_ip_by_id, delete_blacklist_url_by_id,
-    edit_blacklist_ip_by_id, edit_blacklist_url_by_id, get_all_blacklist_ip, get_all_blacklist_url,
-    get_blacklist_ip_by_id, get_blacklist_url_by_id, is_blacklist_ip, is_blacklist_url, signin,
+    add_blacklist_ip,
+    add_blacklist_url,
+    check_rate_limit, // Import the check_rate_limit handler
+    delete_blacklist_ip_by_id,
+    delete_blacklist_url_by_id,
+    edit_blacklist_ip_by_id,
+    edit_blacklist_url_by_id,
+    get_all_blacklist_ip,
+    get_all_blacklist_url,
+    get_blacklist_ip_by_id,
+    get_blacklist_url_by_id,
+    is_blacklist_ip,
+    is_blacklist_url,
+    signin,
     signup,
 };
 
 pub fn configure_routes(cfg: &mut web::ServiceConfig) {
     cfg
+        // Rate limiting endpoint
+        .service(web::resource("/check-rate-limit").route(web::post().to(check_rate_limit)))
         // Blacklist IP endpoints
         .service(
             web::resource("/blacklist-ip")
-                .route(web::post().to(add_blacklist_ip).wrap(JwtAuth))
+                .route(web::post().to(add_blacklist_ip))
                 .route(web::get().to(get_all_blacklist_ip)),
         )
         .service(
             web::resource("/blacklist-ip/{id}")
                 .route(web::get().to(get_blacklist_ip_by_id))
-                .route(web::delete().to(delete_blacklist_ip_by_id).wrap(JwtAuth))
-                .route(web::put().to(edit_blacklist_ip_by_id).wrap(JwtAuth)),
+                .route(web::delete().to(delete_blacklist_ip_by_id))
+                .route(web::put().to(edit_blacklist_ip_by_id)),
         )
         .service(web::resource("/check-blacklist-ip").route(web::post().to(is_blacklist_ip)))
         // Blacklist URL endpoints
         .service(
             web::resource("/blacklist-url")
-                .route(web::post().to(add_blacklist_url).wrap(JwtAuth))
+                .route(web::post().to(add_blacklist_url))
                 .route(web::get().to(get_all_blacklist_url)),
         )
         .service(
             web::resource("/blacklist-url/{id}")
                 .route(web::get().to(get_blacklist_url_by_id))
-                .route(web::delete().to(delete_blacklist_url_by_id).wrap(JwtAuth))
-                .route(web::put().to(edit_blacklist_url_by_id).wrap(JwtAuth)),
+                .route(web::delete().to(delete_blacklist_url_by_id))
+                .route(web::put().to(edit_blacklist_url_by_id)),
         )
         .service(web::resource("/check-blacklist-url").route(web::post().to(is_blacklist_url)))
         // User endpoints
