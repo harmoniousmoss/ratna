541b67f3ac86d2184d369f764550920b276a21fc
Harmoniousmoss <syaefulbahri@protonmail.com>
2024-08-11 14:10:40 +0700

    Add rate limit

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,6 +1,11 @@
 pub mod blacklisted_ip;
 pub use blacklisted_ip::BlacklistedIp;
+
 pub mod malicious;
 pub use malicious::MaliciousUrl;
+
 pub mod brigatory_users;
 pub use brigatory_users::BrigatoryUser;
+
+pub mod rate_limit;
+pub use rate_limit::RateLimitEntry; // Add this line to include the rate limit model
